488dcdab7d0a3842cf1cd8ccc1fd0126543f13fd
amitramesh <amit.ramesh.sharma@gmail.com>
2019-12-28 14:20:07 +0530

    Add New Azure Function Product Api

---
+++ SB.Product.AzureFunction.Api/SB.AzureFunction.Database/Function1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace SB.AzureFunction.Database
+{
+    public static class ProductApi
+    {
+
+        [FunctionName("GetProducts")]
+        public static async Task<IActionResult> GetAllProducts(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/products")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+
+    }
+}
