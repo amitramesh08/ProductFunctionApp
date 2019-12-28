9c05573ae11b6799d1be86704bb1342571b2d3d7
amitramesh <amit.ramesh.sharma@gmail.com>
2019-12-28 21:00:12 +0530

    Remove Unused Namespace

--- SB.Product.AzureFunction.Api/SB.AzureFunction.Product.Api/Function1.cs
+++ SB.Product.AzureFunction.Api/SB.AzureFunction.Product.Api/Function1.cs
@@ -1,14 +1,11 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using SB.ProductApi.Database.Repository;
-using SB.ProductApi.Database.Models;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -39,12 +36,16 @@ namespace SB.AzureFunction.Product.Api
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "products/{productId}")] HttpRequest req,
             ILogger log, string productId)
         {
-            var prod = productRepository.GetProducts().Single(x => x.ProductId == int.Parse(productId));
-            //var products = new List<SB.ProductApi.Database.Models.Product>(prod);
-
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            return (ActionResult)new OkObjectResult(prod);
+            try
+            {
+                var prod = productRepository.GetProducts().Single(x => x.ProductId == int.Parse(productId));
+                log.LogInformation("C# HTTP trigger function processed a request.");
+                return (ActionResult)new OkObjectResult(prod);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex);
+            }
         }
     }
 }
